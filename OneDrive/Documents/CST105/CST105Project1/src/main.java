6a840737f4c1c5aa2c9ceaa6672af85825ff9d96
Brayton Kerekffy <mesadog14@gmail.com>
2018-10-16 15:00:26 -0700

    all of my turned in CS assignments so far

---
+++ OneDrive/Documents/CST105/CST105Project1/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+	public static void main(String []args) {
+		
+		String name = "Brayton Kerekffy";
+		String favColor = "Blue";
+		String lotteryDecision = "Thank God!";
+		
+		System.out.println("My name is " + name);
+		System.out.println("My favorite color is " + favColor);
+		System.out.println("If I won the lottery, I would " + lotteryDecision);
+	}
+}
